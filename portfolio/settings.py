5d5995e353dd3d8cb739d72a4a967e2702945379
fimanishi <fimanishi@gmail.com>
2017-05-09 14:24:49 -0500

    added static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/', '/static/images/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static-compiled")
